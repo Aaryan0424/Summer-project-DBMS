46a33bfe75eac874abc60dc87bdbe9d9e2928a29
Varun <agrawalvarun65@gmail.com>
2022-05-19 22:18:04 +0530

    Added Mongo Atlas

--- Backend/app.js
+++ Backend/app.js
@@ -19,60 +19,64 @@ app.post("/register",function(req,res){
     var password=req.body.password;
     var name=req.body.name;
     var age=req.body.age;
-    var number=req.body.number
-    var MongoClient=require('mongodb').MongoClient
-    var url= "mongodb://127.0.0.1:27017/";
-    MongoClient.connect(url,function(err,db){
-        if(err) throw err;
-        var dbo=db.db("Users");
-        dbo.collection("Details").findOne({"email":emailID},function(err,result){
+    var number=req.body.number;
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    var uri= "mongodb+srv://User:35WTQ4FNBilOpBcO@cluster0.hago4.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    client.connect(err => {
+    const collection = client.db("Cluster0").collection("Details");
+        collection.findOne({"email":emailID},function(err,result){
             if (err) throw err;
             if(result==null){
-                dbo.collection("Details").insert({
-                    "name":name,
-                    "email":emailID,
-                    "password":password,
-                    "age":age,
-                    "number":number
-                 },function(err,result){
-                     if (err) throw err;
-                 });
-                res.send("Registered Succesfully!");
+                collection.insertOne({
+                                "name":name,
+                                "email":emailID,
+                                "password":password,
+                                "age":age,
+                                "number":number
+                            },function(err,result){
+                                if (err) throw err;
+                            });
+                            res.send("Registered Succesfully!");
             }
             else{
                 res.send(emailID+" is already in use!");
             }
-            console.log(result);
+                        
         });
-        //db.close();
+    //client.close();
     });
-   
 });
 app.post("/login",function(req,res){
     console.log(req.body);
-    //res.send("Log in Succesfull!");
-    var emailID=req.body.email;
-    var password=req.body.password;
-    var MongoClient=require('mongodb').MongoClient
-    var url= "mongodb://127.0.0.1:27017/";
-    MongoClient.connect(url,function(err,db){
-        if(err) throw err;
-        var dbo=db.db("Users");
-        dbo.collection("Details").findOne({"email":emailID},function(err,result){
+    const emailID=req.body.email;
+    const password=req.body.password;
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://User:35WTQ4FNBilOpBcO@cluster0.hago4.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    client.connect(err => {
+        const collection = client.db("Cluster0").collection("Details");
+        //console.log(emailID);
+        collection.findOne({"email":emailID},function(err,result){
             if (err) throw err;
+            //console.log(result.password);
+            // console.log(result);
+            // console.log(password);
             if(result==null){
                 res.send("Invalid Username or Password!");
             }
             else if(result.password==password){
-                res.send("Logged In succesfully. Hello"+result.name);
+               
+                res.send("Logged In succesfully. Hello "+result.name);
             }
             else {
                 res.send("Invalid Password or Username");
             }
-            console.log(result);
+            //console.log(result);
         });
-        //db.close();
+    //client.close();
     });
+
 });
 
 app.listen(3000,function(req,res){
