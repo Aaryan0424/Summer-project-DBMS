4379de9fcd297e5bf1b033d7c565f9a14bbec3d4
Aaryan <aaryangupta0424@gmail.com>
2022-06-06 22:51:27 +0530

    Changed imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import Login from "./User/Login";
-import Register from "./User/Register";
-import Home from "./Home/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/Home" element = {<Home />} />
+        <Route exact path="/" element = {<Home />} />
         <Route exact path="/Register" element={<Register />} />
         <Route exact path="/Login" element={<Login />} />
       </Routes>
