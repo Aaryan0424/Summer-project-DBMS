439332e026babd151ba5d162367200c565520a98
Aaryan <aaryangupta0424@gmail.com>
2022-06-06 23:13:20 +0530

    Logic for logout added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "../styles/Navbar.css";
 
+import { useSelector ,useDispatch } from "react-redux";
+import { createBrowserHistory } from "history";
+
 const Navbar = () => {
   document.body.style.background = "#47555E";
+  const userLogin = useSelector((state) => state.userLogin);
+    
+  const { loading , error , userInfo } = userLogin;
+
+  const check = () =>{
+    if(userInfo){
+      return true;
+    }
+    else
+    return false;
+  }
+  const Logouthandler = (event) => { 
+    localStorage.removeItem('userInfo');
+  }
   return (
     <nav className="nav">
       <div class="container">
@@ -12,11 +29,15 @@ const Navbar = () => {
               <a href="/">About</a>
             </li>
             <li>
-              <a href="/Login">Login</a>
+            { check() ? <a href ="http://localhost:5000/logout"  onClick={Logouthandler}>Logout</a> 
+            :
+            <a href="/login"> Login </a> }
             </li>
-            <li>
+            {check() ? "" : 
+              <li>
               <a href="/Register">Register</a>
-            </li>
+              </li>
+            }
             <li>
               <a href="/">Contact-Us</a>
             </li>
