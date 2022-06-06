3fb32689a6487f759e2ceab2d7167bd19e9fdb0b
Aaryan <aaryangupta0424@gmail.com>
2022-06-06 22:53:16 +0530

    User actions added

---
+++ frontend/src/actions/UserActions.js
@@ -0,0 +1,25 @@
+import { USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from "../constants/Userconstants";
+import axios from "axios";
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_LOGIN_REQUEST });
+        const config  = {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+        const { data } = await axios.post("http://localhost:5000/api", { email, password }, config);
+
+        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+        localStorage.setItem('userInfo',JSON.stringify(data));
+    } catch (error) {
+        dispatch({ 
+            type: USER_LOGIN_FAIL, 
+            payload: 
+            error.respone && error.response.data.message ? 
+            error.response.data.message 
+            : error.message 
+        });
+    }
+};
