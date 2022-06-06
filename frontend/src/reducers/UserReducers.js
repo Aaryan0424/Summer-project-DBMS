ed26c8bfb80a4c68a29065b41f61c9394167ee66
Aaryan <aaryangupta0424@gmail.com>
2022-06-06 22:55:12 +0530

    Added reducer for user

---
+++ frontend/src/reducers/UserReducers.js
@@ -0,0 +1,27 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from "../constants/Userconstants";
+
+import { createReducer } from "@reduxjs/toolkit";
+
+export const UserLoginReducer = createReducer([], (builder) => {
+  builder
+    .addCase(USER_LOGIN_REQUEST, (state, action) => {
+      return { loading: true };
+    })
+    .addCase(USER_LOGIN_SUCCESS, (state, action) => {
+      return { loading: false, userInfo: action.payload };
+    })
+    .addCase(USER_LOGIN_FAIL, (state, action) => {
+      return { loading: false, error: action.payload };
+    })
+    .addCase(USER_LOGOUT, (state, action) => {
+      return {};
+    })
+    .addDefaultCase((state, action) => {
+        return state;
+    })
+});
