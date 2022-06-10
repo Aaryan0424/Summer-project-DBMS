614645247bf4770091e1a4a4b0314300f8d20491
Aaryan <aaryangupta0424@gmail.com>
2022-06-10 15:41:53 +0530

    Added reducers for registration

--- frontend/src/reducers/UserReducers.js
+++ frontend/src/reducers/UserReducers.js
@@ -2,7 +2,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
-  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/Userconstants";
 
 import { createReducer } from "@reduxjs/toolkit";
@@ -16,6 +18,22 @@ export const UserLoginReducer = createReducer([], (builder) => {
       return { loading: false, userInfo: action.payload };
     })
     .addCase(USER_LOGIN_FAIL, (state, action) => {
+      return { loading: false, error: action.payload };
+    })
+    .addDefaultCase((state, action) => {
+        return state;
+    })
+});
+
+export const UserRegisterReducer = createReducer([], (builder) => {
+  builder
+    .addCase(USER_REGISTER_REQUEST, (state, action) => {
+      return { loading: true };
+    })
+    .addCase(USER_REGISTER_SUCCESS, (state, action) => {
+      return { loading: false, userInfo: action.payload };
+    })
+    .addCase(USER_REGISTER_FAIL, (state, action) => {
       return { loading: false, error: action.payload };
     })
     .addDefaultCase((state, action) => {
