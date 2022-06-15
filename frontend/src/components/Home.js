3c7ea4683f5243f5fa034e066f77656ff6e71e07
Aaryan <aaryangupta0424@gmail.com>
2022-06-15 19:37:07 +0530

    Changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Carousel from "./Carousel";
+import Products from "./Products";
 import "../styles/Navbar.css"; 
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="main">
       <Navbar/>
-    </div>
+     <div className="front">
+      <div className="front-text">Welcome</div>
+      </div>
+      <div className="deals">
+        <div className="deals-text">Our best deals of the Day!!!</div>
+      </div>   
+      <Carousel/>
+      <Products/>
+      </div>
   );
 };
 
