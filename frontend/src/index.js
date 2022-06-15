06b66349ece814ecf6859f54c06b431fdb89f1d5
Aaryan <aaryangupta0424@gmail.com>
2022-06-15 19:35:55 +0530

    Bootstrap and tailwind changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
