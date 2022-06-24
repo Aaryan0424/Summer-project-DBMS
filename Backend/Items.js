3d2552d1f856b5287994c330700a7a34b7b6352a
Aaryan <aaryangupta0424@gmail.com>
2022-06-24 20:36:31 +0530

    Random list of products

---
+++ Backend/Items.js
@@ -0,0 +1,48 @@
+const Items = 
+[
+    {
+        _id : "1",
+        name : "Item 1",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/belt.webp",
+        description : "This is item 1",
+        price : "10",
+
+    },
+    {
+        _id : "2",
+        name : "Item 2",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(4).webp",
+        description : "This is item 2",
+        price : "20",
+    },
+    {
+        _id : "3",
+        name : "Item 3",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/shoes%20(3).webp",
+        description : "This is item 3",
+        price : "30",
+    },
+    {
+        _id : "4",
+        name : "Item 4",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(23).webp",
+        description : "This is item 4",
+        price : "40",
+    },
+    {
+        _id : "5",
+        name : "Item 5",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(17).webp",
+        description : "This is item 5",
+        price : "50",
+    },
+    {
+        _id : "6",
+        name : "Item 6",
+        img : "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(30).webp",
+        description : "This is item 6",
+        price : "60",
+    }
+];
+
+export default Items;
