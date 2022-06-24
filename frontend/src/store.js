bd2e61027516420c4bf4ca32e9e86276ac2b7cc8
Aaryan <aaryangupta0424@gmail.com>
2022-06-24 20:34:25 +0530

    Added product reducer to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,19 +3,22 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { applyMiddleware, combineReducers } from 'redux';
 import { UserLoginReducer , UserRegisterReducer } from './reducers/UserReducers';
+import { ProductListReducer } from "./reducers/ProductReducer";
 
 const userInfoFromLocalStorage = localStorage.getItem('userInfo') 
 ? JSON.parse(localStorage.getItem('userInfo')) 
 : null;
 
 const initialState = {
-    userLogin : { userInfo : userInfoFromLocalStorage }
+    // productList : {items : []},
+    userLogin : { userInfo : userInfoFromLocalStorage },
 }
 const middleware = [thunk];
 
 
 const Store = configureStore({
     reducer: combineReducers({
+        productList: ProductListReducer,
         userLogin: UserLoginReducer,
         userRegister: UserRegisterReducer,
     }),
